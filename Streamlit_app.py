fc7b2f28303befd73095f72a0532a35d024d9526
Benson Munene <60895430+BensonMunene@users.noreply.github.com>
2025-03-01 16:10:09 +0300

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -4,8 +4,6 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objs as go
 import datetime
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 # ==============================================
 # Page Configuration
